8bb369d3663ed5e16d16e27d5323de5a93aa05a2
zhangzheng <=122362203@qq.com>
2019-08-22 14:07:22 +0800

    '1'

--- mine/web/src/main/java/com/study/mine/web/demo/controller/TestController.java
+++ mine/web/src/main/java/com/study/mine/web/demo/controller/TestController.java
@@ -1,4 +1,23 @@
 package com.study.mine.web.demo.controller;
 
-public class TestController {
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class TestController implements test{
+
+    @RequestMapping("/test")
+    public String test(){
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = servletRequestAttributes.getRequest();
+        System.out.println(request.getLocalPort());
+        System.out.println(request.getRemotePort());
+        System.out.println(request.getServerPort());
+        return "使用的端口号是:"+request.getLocalPort();
+    }
 }
--- mine/web/src/main/java/com/study/mine/web/demo/controller/test.java
+++ mine/web/src/main/java/com/study/mine/web/demo/controller/test.java
@@ -1,4 +1,9 @@
 package com.study.mine.web.demo.controller;
 
 public interface test {
+
+    default void testabb(){
+
+        System.out.println(1);
+    };
 }
