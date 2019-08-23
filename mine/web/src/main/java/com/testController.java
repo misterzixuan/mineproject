02ced2df9b87871b45c1bcfffeab292cc9696960
zhangzheng <=122362203@qq.com>
2019-08-24 02:08:19 +0800

    'cookie实现分布式session共享问题'

--- mine/web/src/main/java/com/testController.java
+++ mine/web/src/main/java/com/testController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
@@ -20,17 +22,34 @@ public class testController {
     public Object test(){
         ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = servletRequestAttributes.getRequest();
+        HttpServletResponse response = servletRequestAttributes.getResponse();
+
         HttpSession session = request.getSession(true);
+        Cookie[] cookies = request.getCookies();
+        String name="port";
         System.out.println(session);
         int localPort = request.getLocalPort();
         if(localPort==8080){
             session.setAttribute("port0","8080");
-        }if(localPort==8081){
+
+            Cookie cookie=new Cookie("port","aaaaaaaaa");
+            response.addCookie(cookie);
+        }
+        if(localPort==8081){
             session.setAttribute("port1","8081");
         }
         if(localPort==8082){
             session.setAttribute("port2","8082");
         }
+        String cvalue="";
+        for (Cookie cookie:cookies
+        ) {
+            String realName=cookie.getName();
+            if(realName.equals(name)){
+                cvalue= cookie.getValue();
+            }
+        }
+
 
         JSONObject result=new JSONObject();
         String value0= (String) session.getAttribute("port0");
@@ -39,6 +58,7 @@ public class testController {
         result.put("8080",value0);
         result.put("8081",value1);
         result.put("8082",value2);
+        result.put("cooke实现session共享",cvalue);
         result.put("当前端口是",request.getLocalPort());
         System.out.println("获取session中的值"+request.getLocalPort());
         return result;
