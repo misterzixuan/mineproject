6c46aaf971961c2ea9ebf17eba6eca72b91dd5f3
zhangzheng <=122362203@qq.com>
2019-08-26 09:48:11 +0800

    '222'

--- mine/web/src/main/java/com/tomcatsessionproblem/testController.java
+++ mine/web/src/main/java/com/tomcatsessionproblem/testController.java
@@ -20,6 +20,7 @@ public class testController {
     @RequestMapping("/test")
     public Object test(){
         System.out.println(1111);
+        System.out.println(2222);
         ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = servletRequestAttributes.getRequest();
         HttpServletResponse response = servletRequestAttributes.getResponse();
