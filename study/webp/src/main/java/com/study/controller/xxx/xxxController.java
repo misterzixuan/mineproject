167074ea34a7a4b6be908652d6e19e0c4acea180
zhangzheng <=122362203@qq.com>
2019-09-09 09:46:20 +0800

    1

--- study/webp/src/main/java/com/study/controller/xxx/xxxController.java
+++ study/webp/src/main/java/com/study/controller/xxx/xxxController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.nio.file.Files;
+
 
 @RestController
 @RequestMapping("xxx")
@@ -39,9 +42,9 @@ public class xxxController {
         System.out.println(errorInfo);
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("1044984012@qq.com");
-        message.setTo("122362203@qq.com");
-        message.setSubject("主题：简单邮件1");
-        message.setText("测试邮件内容1");
+        message.setCc("1760180402@qq.com");
+        message.setSubject("主题：发给猪的一份信");
+        message.setText("不知道猪能不能看懂！！");
         mailSender.send(message);
         mailSender.send(message);
         return "没有异常";
